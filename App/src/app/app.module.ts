5d98bbb240ae9fb65b64bec51e86eb3204b75406
Javier <javierru8@outlook.es>
2022-04-01 19:15:22 +0200

    v1.0

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RoutineComponent } from './components/routine/routine.component';
+import { RoutinesComponent } from './components/routines/routines.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutineComponent,
+    RoutinesComponent
   ],
   imports: [
     BrowserModule
